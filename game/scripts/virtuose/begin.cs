30ab383398b299635f1df886a1cdc91f944eb0e3
bcronertrevenge <zbrunoz@hotmail.fr>
2014-05-06 12:11:45 +0200

    Xflutes est ready !

--- game/scripts/virtuose/begin.cs
+++ game/scripts/virtuose/begin.cs
@@ -89,7 +89,7 @@ function placeInstruments(%nbInstruments){
 }
 
 function createInstruments(%instrument,%file){
-				
+				echo(%instrument SPC %file);
 				%nameInstru=%instrument;
 
 				
@@ -201,7 +201,7 @@ function createInstruments(%instrument,%file){
 }
 
 function placeInstrumentsTrack(){
-	for(%i=1;%i<$orchestrator.getNumTracks();%i++){
+	for(%i=0;%i<$orchestrator.getNumTracks();%i++){
 		//echo($orchestrator.getTrackName(%i));
 		//echo($orchestrator.getInstrumentName(%i));
 		createInstruments($orchestrator.getTrackName(%i),$orchestrator.getInstrumentName(%i));
